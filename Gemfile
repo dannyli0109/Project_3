7dadb177e3b2b0625e7deae19232d09d39b58ae8
Danny Li <dannyli0109@gmail.com>
2017-07-25 13:39:31 +1000

    trending list

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ git_source(:github) do |repo_name|
 end
 
 
+gem 'oauth'
+gem 'json'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.2'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/", to: "pages#index"
+  get "/api/trends/:id", to: 'api/trends#show'
+
+  get "/pages/:name", to: "pages#show"
 end
